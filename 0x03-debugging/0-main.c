d840cccc7f63351031f2c6bb9c7748b9085469a6
Eleazar Ahiahornu <ahiahornuelea@gmail.com>
2023-07-20 08:42:14 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
-#ifndef MAIN_H
+#include "main.h"
 
-#define MAIN_H
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
+int main(void)
+{
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
 
-#include <stdio.h>
-
-int largest_number(int a, int b, int c);
-
-void positive_or_negative(int i);
-
-void print_remaining_days(int month, int day, int year);
-
-int convert_day(int month, int day);
-
-#endif
+	return (0);
+}
